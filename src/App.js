6f89efb992528c89d5c88a6f9b432893c7b06909
Ashik <ashiknarayansappandi@users.noreply.github.com>
2020-07-08 20:13:13 +0530

    Passed heading to Header component as props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from "./Header";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+
+        let subscribers = [
+            {
+                id: 1,
+                name: 'Ashik',
+                phone: '9878945676'
+            },
+            {
+                id: 2,
+                name: 'Anita',
+                phone: '7456789244'
+            }
+        ]
+
+        return (
+            <div className='main-page'>
+                <Header heading="Phone Directory"/>
+                <div className='main-body'>
+                    <div className='layout-top'>
+                        <button className='btn btn-add'> Add</button>
+                    </div>
+                    <div className='layout-middle'>
+                        <div className='subscriber-item'>
+                            <span className='col col-head col-name'>Name</span>
+                            <span className='col col-head col-phone'>Phone</span>
+
+                        </div>
+                        {
+                            subscribers.map(sub => {
+                                return(
+                                    <div className='subscriber-item' key={sub.id}>
+                                        <span className='col col-data col-name'>{sub.name}</span>
+                                        <span className='col col-data col-phone'>{sub.phone}</span>
+                                        <button className='btn btn-delete'>Delete</button>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div className='main-footer'>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import './Header.css'
+
+const Header = function (props) {
+    return (
+        <div className='header'>
+            <h1>{props.heading}</h1>
+        </div>
+    )
+}
+
+export default Header;
